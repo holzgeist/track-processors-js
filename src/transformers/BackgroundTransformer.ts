af3b237f86b60266eeded200c2946e0dbb7aba2c
Tobias Ollmann <tobias.ollmann@instahelp.me>
2025-08-05 15:23:01 +0200

    fix: don't fail without radius/background image

--- src/transformers/BackgroundTransformer.ts
+++ src/transformers/BackgroundTransformer.ts
@@ -109,7 +109,12 @@ export default class BackgroundProcessor extends VideoTransformer<BackgroundOpti
         return;
       }
 
-      if (this.isDisabled) {
+      let disabled = this.isDisabled ?? false;
+      if (this.options.blurRadius === null && this.options.imagePath === null) {
+        disabled = true;
+      }
+
+      if (disabled) {
         controller.enqueue(frame);
         return;
       }
